03301075aae6f5ae60d3773da68a07d4a9d4390c
grcetmpk <grace.tompkins97@gmail.com>
2025-10-28 10:38:23 -0700

    adding tests

--- R/square.R
+++ R/square.R
@@ -1,3 +1,15 @@
+#' @title Squares numeric data
+#'
+#' @description Squares numeric data (single a number, or a numeric vector).
+#'
+#' @param x numeric, vector
+#' @returns numeric, vector
+#'
+#' @examples
+#' square(5)
+#' square(0)
+#' square(c(1,2,3))
+#'
 #' @export
 square <- function(x){
 
